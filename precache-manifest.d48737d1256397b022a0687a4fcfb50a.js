bc90d1e566d0469aa85e66da0c174acea3527045
unknown <kousic.it@gmail.com>
2020-09-27 17:02:06 +0530

    Updates

---
+++ precache-manifest.d48737d1256397b022a0687a4fcfb50a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3517e4521865d21b37f77e145c5b28c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538b373d8e30f438d9ee",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "a2b64aaf36dcfcb74194",
+    "url": "/static/css/main.15b78528.chunk.css"
+  },
+  {
+    "revision": "538b373d8e30f438d9ee",
+    "url": "/static/js/2.d635db59.chunk.js"
+  },
+  {
+    "revision": "8cdd8cbd41aa56d58bbfb4353c6f99a0",
+    "url": "/static/js/2.d635db59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b64aaf36dcfcb74194",
+    "url": "/static/js/main.d9346eb5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e096600cb6ba1a2aa6bf4e18272658e1",
+    "url": "/static/media/Kousic.e096600c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cf64b4c8cf87869f99daa66da474183b",
+    "url": "/static/media/img.cf64b4c8.jpg"
+  }
+]);
